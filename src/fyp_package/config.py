correct mapping from contact orientation to z rotation for both environments

src/fyp_package/config.py
@@ -2,7 +2,7 @@ import numpy as np
 from fyp_package.utils import euler2quat, rot2quat, tf
 import os
 
-simulation = True
+simulation = False
 
 #### Camera
 
@@ -11,6 +11,7 @@ if simulation:
     camera_position = (0, -0.85, 0.4)
     camera_orientation = (np.pi / 4 + np.pi / 48, np.pi, np.pi)
     camera_orientation_q = euler2quat(*camera_orientation)
+    cam2base_tf = tf(camera_orientation_q, camera_position)
     zrange = (0.01, 10.)
 
     camera_image_size = (480, 480)
@@ -102,6 +103,7 @@ real_object_list = ["espresso cup", "bowl"]
 
 robot_type = "m1n6s200"
 robot_ready_position = [0, -0.25, 0.25]
+# If modifying the orientation, check grasp transform still works
 robot_vertical_orientation_e = [np.pi, 0, 0]
 robot_vertical_orientation_q = euler2quat(*robot_vertical_orientation_e)
 robot_tucked_position = [0, -0.2, 0.35]

src/fyp_package/environment.py
@@ -78,7 +78,16 @@ class PhysicalEnvironment(Environment):
     def move_robot(self, position=config.robot_ready_position, orientation=None, relative=False):
         return self.robot.move_robot(position, orientation, relative)
 
-    def put_first_on_second(self, pick_pos, place_pos):
+    # pick orientation is a scalar representing the angle of rotation around the z-axis
+    # it should be in the range [-pi/2, pi/2]
+    def put_first_on_second(self, pick_pos, place_pos, pick_orientation=None):
+        if pick_orientation is None:
+            pick_orientation = config.robot_vertical_orientation_q
+        else:
+            rotation_euler = [0, 0, pick_orientation]
+            pick_orientation = utils.rot2quat(utils.quat2rot(config.robot_vertical_orientation_q) @ utils.quat2rot(utils.euler2quat(*rotation_euler)))
+
+
         pick_pos = np.array(pick_pos)
         place_pos = np.array(place_pos)
         # Set fixed primitive z-heights.
@@ -87,7 +96,6 @@ class PhysicalEnvironment(Environment):
             pick_xyz = np.append(pick_pos, 0.025)
         else:
             pick_xyz = pick_pos
-            pick_xyz[2] = 0.025
         if place_pos.shape[-1] == 2:
             place_xyz = np.append(place_pos, 0.15)
         else:
@@ -97,11 +105,11 @@ class PhysicalEnvironment(Environment):
         # Move to object.
         ee_xyz = self.get_ee_pose()[0]
         while np.linalg.norm(hover_xyz - ee_xyz) > 0.03:
-            self.move_robot(hover_xyz)
+            self.move_robot(hover_xyz, pick_orientation)
             ee_xyz = self.get_ee_pose()[0]
 
         while np.linalg.norm(pick_xyz - ee_xyz) > 0.03:
-            self.move_robot(pick_xyz)
+            self.move_robot(pick_xyz, pick_orientation)
             ee_xyz = self.get_ee_pose()[0]
 
         # Pick up object.
@@ -161,16 +169,22 @@ class SimulatedEnvironment(Environment):
 
     def open_gripper(self):
         self.sim.gripper.release()
+        for _ in range(240):
+            self.sim.step_sim_and_render()
 
     def close_gripper(self):
         self.sim.gripper.activate()
+        for _ in range(240):
+            self.sim.step_sim_and_render()
 
-    def move_robot(self, position):
-        self.sim.move_robot(position)
+    def move_robot(self, position, orientation=None, relative=False):
+        if relative:
+            position = np.array(position) + np.array(self.get_ee_pose()[0])
+        self.sim.move_ee(position, orientation)
         return self.get_ee_pose()
 
-    def put_first_on_second(self, pick_pos, place_pos):
-        self.sim.step(action={'pick': np.array(pick_pos), 'place': np.array(place_pos)})
+    def put_first_on_second(self, pick_pos, place_pos, pick_orientation=None):
+        self.sim.step(action={'pick': np.array(pick_pos), 'place': np.array(place_pos), 'pick_orientation': pick_orientation})
         return True
 
     def get_images(self, save=False, save_path_rgb=config.latest_rgb_image_path, save_path_depth=config.latest_depth_image_path):
@@ -195,14 +209,28 @@ if __name__ == "__main__":
     # simulated_env.move_robot([0, 0, 0])
     # simulated_env.open_gripper()
 
+
+
+
     env = SimulatedEnvironment(3, 3)
-    # block = env.sim.get_obj_pos(f'{colour} block')
-    # bowl = env.sim.get_obj_pos(f'{colour} bowl')
     rgb, depth = env.get_images(save=True)
-    import matplotlib.pyplot as plt
-    plt.imshow(rgb)
-    plt.show()
+    print(env.get_ee_pose())
+
+    current_quat = env.get_ee_pose()[1]
+    while True:
+        euler = eval(input("Enter euler angles: "))
+        print(utils.quat2euler(env.get_ee_pose()[1]))
+        new_quat = utils.rot2quat(utils.quat2rot(current_quat) @ utils.quat2rot(utils.euler2quat(*euler)))
+
+        env.move_robot([0, -0.5, 0.2], new_quat)
+        env.move_robot([0, -0.5, 0.3], new_quat)
+        env.move_robot([0, -0.5, 0.2], new_quat)
+        env.move_robot([0, -0.5, 0.3], new_quat)
+
+        print(utils.quat2euler(new_quat))
 
 
     colour = input("Press Enter to pick and place")
+    # block = env.sim.get_obj_pos(f'{colour} block')
+    # bowl = env.sim.get_obj_pos(f'{colour} bowl')
     # env.put_first_on_second(block, bowl)

src/fyp_package/experiments/CAP_vision_agent.py
@@ -453,15 +453,25 @@ class LMP_wrapper():
 
         grasp2base_tf = config.cam2base_tf @ grasp2cam_tf
 
-        contact_point = config.cam2base_tf @ np.concatenate([contact_point_cam, [1]])
+        contact_point = config.cam2base_tf @ np.concatenate([contact_point_cam, [1]])[:3]
+        contact_point[2] -= 0.03
+
         grasp_position = utils.tf_trans(grasp2base_tf)
         grasp_orientation = utils.tf_rot(grasp2base_tf)
+        grasp_orientation = utils.rot2quat(grasp_orientation)
+        grasp_z_rot = -utils.quat2euler(grasp_orientation)[2]
+        # wrap rot to [-pi/2, pi/2]
+        if grasp_z_rot > np.pi/2:
+            grasp_z_rot -= np.pi
+        elif grasp_z_rot < -np.pi/2:
+            grasp_z_rot += np.pi
 
         print("Detected grasp with contact point:", contact_point)
+        print("Grasp rotation about z-axis:", grasp_z_rot)
         # print("Detected grasp with position:", grasp_position)
         # print("Detected grasp with orientation:", grasp_orientation)
 
-        return contact_point # , grasp_position, grasp_orientation
+        return contact_point, grasp_z_rot # , grasp_position, grasp_orientation
 
 
 # %% [markdown]

src/fyp_package/model_client.py
@@ -64,6 +64,8 @@ class ModelClient:
         '''
         data = (depth_path, rgb_path, mask_path)
         response = self.send_request('graspnet', data)
+        if response is None:
+            return None
         best_grasp_cam, best_score, best_contact_pt = response
         if save:
             np.savez(save_path, pred_grasp_cam=best_grasp_cam, score=best_score, contact_pt=best_contact_pt)
@@ -97,7 +99,12 @@ if __name__ == "__main__":
     client = ModelClient()
     # Example usage
     try:
-        masks, boxes, phrases = client.langsam_predict(config.latest_rgb_image_path, "white bowl", save=True)
+        from fyp_package import environment
+        import pybullet as pb
+        env = environment.PhysicalEnvironment()
+        env.get_images(save=True)
+
+        masks, boxes, phrases = client.langsam_predict(config.latest_rgb_image_path, "paper cup", save=True)
         print(masks, boxes, phrases)
         np.save(config.chosen_segmentation_mask_path, masks[0])
         plt.imshow(np.load(config.latest_segmentation_masks_path)[0])
@@ -106,11 +113,39 @@ if __name__ == "__main__":
         # from fyp_package import object_detection_utils
         # object_detection_utils.get_object_cube_from_segmentation(masks, phrases, cv2.imread(config.latest_rgb_image_path), np.load(config.latest_depth_image_path), config.camera_position, config.camera_orientation_q, config.intrinsics)
 
-        best_grasp_cam, best_score, best_contact_pt = client.contact_graspnet_predict(
+        result = client.contact_graspnet_predict(
             config.latest_depth_image_path, config.latest_rgb_image_path, config.chosen_segmentation_mask_path, save=True
         )
-        results = np.load(config.latest_grasp_detection_path)
-        print(results['pred_grasp_cam'], results['score'], results['contact_pt'])
+
+        grasp2cam_tf, _score, contact_point_cam = result
+
+        grasp2base_tf = config.cam2base_tf @ grasp2cam_tf
+        grasp_rot = utils.tf_rot(grasp2base_tf)
+        # rotate grasp_orientation by 90 degrees around z-axis
+        # grasp_orientation = utils.rot2quat(grasp_rot @ utils.quat2rot(utils.euler2quat(*[0, 0, np.pi/2])))
+        grasp_orientation = utils.rot2quat(grasp_rot)
+        grasp_z_rot = -utils.quat2euler(grasp_orientation)[2]
+        # wrap rot to [-pi/2, pi/2]
+        if grasp_z_rot > np.pi/2:
+            grasp_z_rot -= np.pi
+        elif grasp_z_rot < -np.pi/2:
+            grasp_z_rot += np.pi
+
+        contact_point = (config.cam2base_tf @ np.concatenate([contact_point_cam, [1]]))[:3]
+        contact_point[2] -= 0.03
+
+        print(grasp2base_tf, contact_point)
+        print(utils.quat2euler(grasp_orientation))
+
+
+        print(utils.quat2euler(env.get_ee_pose()[1]))
+        
+        env.put_first_on_second(contact_point, config.robot_ready_position, grasp_z_rot)
+
+        input("Press Enter to move")
+        env.move_robot([0, 0, 0.1], relative=True)
+
+
 
     except ConnectionError as e:
         print(e)

src/fyp_package/pick_and_place_env.py
@@ -3,7 +3,7 @@ import pybullet_data
 import threading
 import numpy as np
 import os
-from fyp_package import config
+from fyp_package import config, utils
 
 from time import sleep
 
@@ -137,7 +137,7 @@ class PickPlaceEnv():
     pybullet.setTimeStep(self.dt)
 
     self.home_joints = [-np.pi/2,0.2,0,-1.3,0, 1.6, np.pi/4, 0, 0]  # Joint angles: (J0, J1, J2, J3, J4, J5, J6, finger_left, finger_right).
-    self.home_ee_euler = (np.pi, 0, np.pi)  # (RX, RY, RZ) rotation in Euler angles.
+    self.home_ee_euler = (np.pi, 0, - np.pi / 2)  # (RX, RY, RZ) rotation in Euler angles.
     self.ee_link_id = 8  # Link ID of Panda end effector.
     self.tip_link_id = 11  # Link ID of gripper finger tips. # wanted 11 but it is not appearing
     self.gripper = None
@@ -210,7 +210,7 @@ class PickPlaceEnv():
           object_id = pybullet.createMultiBody(0.01, object_shape, object_visual, basePosition=object_position)
         elif object_type == 'bowl':
           object_position[2] = 0
-          object_id = pybullet.loadURDF(PYBULLET_ASSETS_DIR + "bowl/bowl.urdf", object_position, useFixedBase=1)
+          object_id = pybullet.loadURDF(PYBULLET_ASSETS_DIR + "bowl/bowl.urdf", object_position) #, useFixedBase=True)
         pybullet.changeVisualShape(object_id, -1, rgbaColor=object_color)
         self.obj_name_to_id[obj_name] = object_id
 
@@ -235,7 +235,9 @@ class PickPlaceEnv():
       targetPositions=joints,
       positionGains=[0.01]*len(self.joint_ids))
 
-  def movep(self, position):
+  def movep(self, position, orientation=None):
+    if orientation is None:
+      orientation = pybullet.getQuaternionFromEuler(self.home_ee_euler)
     # add offset to z to account for the gripper, copying as to not modify the original
     position = position.copy()
     position[2] += self.gripper_height
@@ -244,7 +246,7 @@ class PickPlaceEnv():
         bodyUniqueId=self.robot_id,
         endEffectorLinkIndex=self.tip_link_id,
         targetPosition=position,
-        targetOrientation=pybullet.getQuaternionFromEuler(self.home_ee_euler),
+        targetOrientation=orientation,
         maxNumIterations=100)
     self.servoj(joints[:-2]) # Do not move fingers
 
@@ -260,17 +262,19 @@ class PickPlaceEnv():
     ee_pos, ee_quat = pybullet.getLinkState(self.robot_id, self.tip_link_id)[0:2]
     # take offset from z to account for the gripper
     offset = np.array([0, 0, -self.gripper_height])
-    offset_rotated = pybullet.getMatrixFromQuaternion(ee_quat) @ offset
+    rotation = pybullet.getMatrixFromQuaternion(ee_quat)
+    rotation = np.float32(rotation).reshape(3, 3)
+    offset_rotated = rotation @ offset
     ee_pos = np.array(ee_pos) + offset_rotated
     return ee_pos, ee_quat
   
   def get_wrist_pose(self):
     return pybullet.getLinkState(self.robot_id, self.tip_link_id)[0:2]
   
-  def move_ee(self, position):
+  def move_ee(self, position, orientation = None):
     ee_xyz = self.get_ee_pos()
     while np.linalg.norm(position - ee_xyz) > 0.01:
-      self.movep(position)
+      self.movep(position, orientation)
       self.step_sim_and_render()
       ee_xyz = self.get_ee_pos()
 
@@ -278,13 +282,18 @@ class PickPlaceEnv():
     """Do pick and place motion primitive."""
     pick_pos, place_pos = action['pick'].copy(), action['place'].copy()
 
+    pick_orientation = pybullet.getQuaternionFromEuler(self.home_ee_euler)
+    if 'orientation' in action:
+      if action['orientation'] is not None:
+        rotation_euler = [0, 0, action['orientation']]
+        pick_orientation = utils.rot2quat(utils.quat2rot(pick_orientation) @ utils.quat2rot(utils.euler2quat(*rotation_euler)))
+
     # Set fixed primitive z-heights.
     hover_xyz = np.float32([pick_pos[0], pick_pos[1], 0.2])
     if pick_pos.shape[-1] == 2:
-      pick_xyz = np.append(pick_pos, 0.02)
+      pick_xyz = np.append(pick_pos, 0.01)
     else:
       pick_xyz = pick_pos
-      pick_xyz[2] = 0.02
     if place_pos.shape[-1] == 2:
       place_xyz = np.append(place_pos, 0.15)
     else:
@@ -294,12 +303,12 @@ class PickPlaceEnv():
     # Move to object.
     ee_xyz = self.get_ee_pos()
     while np.linalg.norm(hover_xyz - ee_xyz) > 0.01:
-      self.movep(hover_xyz)
+      self.movep(hover_xyz, pick_orientation)
       self.step_sim_and_render()
       ee_xyz = self.get_ee_pos()
 
     while np.linalg.norm(pick_xyz - ee_xyz) > 0.01:
-      self.movep(pick_xyz)
+      self.movep(pick_xyz, pick_orientation)
       self.step_sim_and_render()
       ee_xyz = self.get_ee_pos()
 
@@ -340,9 +349,10 @@ class PickPlaceEnv():
       self.movep(place_xyz)
       self.step_sim_and_render()
       ee_xyz = self.get_ee_pos()
-    place_xyz = np.float32([0, -0.5, 0.2])
-    while np.linalg.norm(place_xyz - ee_xyz) > 0.01:
-      self.movep(place_xyz)
+
+    home_xyz = np.float32([0, -0.5, 0.2])
+    while np.linalg.norm(home_xyz - ee_xyz) > 0.01:
+      self.movep(home_xyz)
       self.step_sim_and_render()
       ee_xyz = self.get_ee_pos()
 