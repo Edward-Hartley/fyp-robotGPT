d7f482aa13964d635b832da62f4b78e920077964
Edward Hartley <edchartley@gmail.com>
2024-06-04 11:41:17 +0100

    removed excessive tabs in prompts

--- src/fyp_package/prompts/robot_agent_prompts.py
+++ src/fyp_package/prompts/robot_agent_prompts.py
@@ -20,16 +20,16 @@ The robot system has the following packages available:
 {packages}
 
 {few_shot_introduction}
-'''.strip()
+'''.strip().replace('\n    ', '\n')
 
 few_shot_introduction = '''
 What follows are some example interactions between you and the user. Please follow the same format.
-'''.strip()
+'''.strip().replace('\n    ', '\n')
 
 gptv_injection_message = '''
 Before outlining my broad objectives in order to complete the user's task, I should carefully check the current environment for any contextual information. I will use the VIEW_SCENE tool to request an image of the scene.
 $$VIEW_SCENE$$
-'''.strip()
+'''.strip().replace('\n    ', '\n')
 
 functions_advice = {
     "vision_assistant": "the vision_assistant() function allows you to delegate perception tasks to an agent who has access to perception models. You can request information about the scene and specify the requested information and return format in the message. You can also request information in the format of a string and print it in order to inform your own decisions. The vision agent is reset upon each query, make sure to include information you already know so it doesn't have to recalculate it.",
@@ -54,7 +54,7 @@ function_docs = {
         left_most_bowl = vision_assistant("Please tell me which bowl is largest in the scene. There are three bowls: two white and one red. Please return a string, such as 'leftmost white bowl' or 'white bowl at position (x, y, z)'.")
         print(left_most_bowl)
         mug_grasp_position, mug_grasp_z_angle = vision_assistant("Please return the position and z angle of how to grasp the ceramic mug. Return a tuple of a list and a float: [x, y, z], z_angle. The mug is located at approximately (0.102, -0.099, 0.031).")
-    ''',
+    '''.strip().replace('\n    ', '\n'),
 
     "move_robot": '''
     Move the robot gripper to a specific position and orientation.
@@ -70,7 +70,7 @@ function_docs = {
     Example:
         new_position, new_orientation = move_robot([0.1, -0.2, 0.3], [0, 0, np.pi/2])
         print(new_position, new_orientation)
-    ''',
+    '''.strip().replace('\n    ', '\n'),
     
     "move_robot_relative": '''
     Move the robot gripper by a relative amount.
@@ -87,7 +87,7 @@ function_docs = {
         # Move the gripper 10cm in the x direction and tilt the gripper by 30 degrees.
         new_position, new_orientation = move_robot_relative([0.1, 0, 0], [0, np.pi/6, 0])
         print(new_position, new_orientation)
-    ''',
+    '''.strip().replace('\n    ', '\n'),
 
     "close_gripper": '''
     Close the gripper.
@@ -97,14 +97,14 @@ function_docs = {
 
     Example:
         close_gripper()
-    ''',
+    '''.strip().replace('\n    ', '\n'),
 
     "open_gripper": '''
     Open the gripper.
     
     Returns:
         None
-    ''',
+    '''.strip().replace('\n    ', '\n'),
 
     "put_first_on_second": '''
     This is a helper function to move an object in one position to another.
@@ -123,19 +123,19 @@ function_docs = {
         for (teabag, mug) in zip(teabag_positions, mug_positions):
             success = put_first_on_second(teabag, mug)
             print("moving teabag to mug: ", success)
-    '''
+    '''.strip().replace('\n    ', '\n'),
 }
 
 final_system_message = '''
 This is the end of the examples. The real user's query will follow. All previous variables no longer exist and you must start from scratch. The demonstration shows you only a very limited way to interact with the environment. You should use your creativity to adapt and solve the user's task.
-'''.strip()
+'''.strip().replace('\n    ', '\n')
 
 check_completion_message = '''
 This is a standard check to ensure that the user's task is complete. It may truly be complete.
 The original task was:
 "{query}"
 An image of the final scene is provided below. First, reason about the user's task and the current state of the scene. Then, if the task is complete you should repeat the COMPLETE tool in your response. If it is not, provide feedback to the robot agent to let it know what is missing.
-'''.strip()
+'''.strip().replace('\n    ', '\n')
 
 missing_tool_use_correction = '''
 Format error: No $$ found in completion. Please make sure you use exactly one of $$CODE$$, $$VIEW_SCENE$$, or $$COMPLETE$$ in your response.
@@ -156,23 +156,23 @@ stack_blocks_all_modules = [
     print("middle block position: ", middle_block_pos)
     success = put_first_on_second(middle_block_pos, largest_block_pos)
     print(success)
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
     largest block position: [0.102, -0.358, 0.116]
     middle block position: [0.062, -0.322, 0.043]
     True
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     I think the blocks were successfully stacked. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows that the green block has moved slightly but is not stacked on the red block.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The blocks are not stacked as expected. I try to more carefully pick up the middle block.
@@ -193,7 +193,7 @@ stack_blocks_all_modules = [
     print("grasp pos: ", move_robot(middle_block_grasp_pos, [0, 0, middle_block_grasp_z_angle]))
     print("gripper: ", close_gripper())
     print("pick up: ", move_robot(hover_pos, [0, 0, middle_block_grasp_z_angle]))
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
@@ -203,17 +203,17 @@ stack_blocks_all_modules = [
     Gripper now closed.
     gripper: None
     pick up:  ([0.069, -0.311, 0.142], [-0.004, 0.002, 0.590])
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     All the movements were successful, I now expect to have the middle block in the gripper.
     I will verify this by checking the scene.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows the middle block in the gripper.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''he vision assistant can help identify the best angle to grasp objects at.
     The middle block is now in the gripper. I will now place it on the largest block. Since the block is already in my gripper I cannot use the put_first_on_second function. I will move the gripper to the position of the largest block and then move it down slightly to place the middle block.
@@ -230,7 +230,7 @@ stack_blocks_all_modules = [
     print("place pos: ", move_robot(place_pos, [0, 0, 0]))
     print("gripper: ", open_gripper())
     print("return to hover: ", move_robot(hover_pos, [0, 0, 0]))
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
@@ -240,16 +240,16 @@ stack_blocks_all_modules = [
     Gripper now open.
     gripper: None
     return to hover:  ([0.102, -0.358, 0.216], [0.0, 0.0, 0.0])
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The middle block was successfully placed on the largest block. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows the middle block successfully placed on the largest block.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The middle block is now placed on the largest block. I am still in step 1, stacking the blocks steadily in order of size.
@@ -261,14 +261,14 @@ stack_blocks_all_modules = [
     print("middle block position: ", middle_block_pos)
     success = put_first_on_second(smallest_block_pos, middle_block_pos)
     print(success)
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
     smallest block position: [-0.109, -0.292, 0.033]
     middle block position: [0.097, -0.359, 0.132]
     True
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 ]
 
 stack_blocks_no_fine_control = [
@@ -286,23 +286,23 @@ stack_blocks_no_fine_control = [
     print("middle block position: ", middle_block_pos)
     success = put_first_on_second(middle_block_pos, largest_block_pos)
     print(success)
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
     largest block position: [0.102, -0.358, 0.116]
     middle block position: [0.062, -0.322, 0.043]
     True
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     I think the blocks were successfully stacked. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows that the green block has moved slightly but is not stacked on the red block.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The blocks are not stacked as expected. I try to more carefully pick up the middle block.
@@ -312,22 +312,22 @@ stack_blocks_no_fine_control = [
     print('middle block grasp: ', middle_block_grasp_pos, middle_block_grasp_z_angle)
     success = put_first_on_second(middle_block_grasp_pos, largest_block_pos, middle_block_grasp_z_angle)
     print(success)
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
     middle block grasp: [0.068, -0.312, 0.043] [0.0, 0.0, 0.589]
     True
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     I think the pick and place was successful. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows green block on top of a red block.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The middle block is now placed on the largest block. I am still in step 1, stacking the blocks steadily in order of size.
@@ -339,14 +339,14 @@ stack_blocks_no_fine_control = [
     print("middle block position: ", middle_block_pos)
     success = put_first_on_second(smallest_block_pos, middle_block_pos)
     print(success)
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
     smallest block position: [-0.109, -0.292, 0.033]
     middle block position: [0.097, -0.359, 0.132]
     True
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 ]
 
 stack_blocks_no_pick_and_place = [
@@ -375,7 +375,7 @@ stack_blocks_no_pick_and_place = [
     print("place pos: ", move_robot(largest_block_pos, [0, 0, 0]))
     print("gripper: ", open_gripper())
     print("move away: ", move_robot(hover_pos, [0, 0, 0]))
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
@@ -391,16 +391,16 @@ stack_blocks_no_pick_and_place = [
     Gripper now open.
     gripper: None
     return to hover:  ([0.102, -0.358, 0.216], [0.0, 0.0, 0.0])
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     I think the blocks were successfully stacked. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows that the green block has moved slightly but is not stacked on the red block.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The blocks are not stacked as expected. I try to more carefully pick up the middle block.
@@ -415,7 +415,7 @@ stack_blocks_no_pick_and_place = [
     print("grasp pos: ", move_robot(middle_block_grasp_pos, [0, 0, middle_block_grasp_z_angle]))
     print("gripper: ", close_gripper())
     print("pick up: ", move_robot(hover_pos, [0, 0, middle_block_grasp_z_angle]))
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
@@ -425,17 +425,17 @@ stack_blocks_no_pick_and_place = [
     Gripper now closed.
     gripper: None
     pick up:  ([0.069, -0.311, 0.142], [-0.004, 0.002, 0.590])
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     All the movements were successful, I now expect to have the middle block in the gripper.
     I will verify this by checking the scene.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows the middle block in the gripper.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The middle block is now in the gripper. I will now place it on the largest block. Since the block is already in my gripper I cannot use the put_first_on_second function. I will move the gripper to the position of the largest block and then move it down slightly to place the middle block.
@@ -452,7 +452,7 @@ stack_blocks_no_pick_and_place = [
     print("place pos: ", move_robot(place_pos, [0, 0, 0]))
     print("gripper: ", open_gripper())
     print("return to hover: ", move_robot(hover_pos, [0, 0, 0]))
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
@@ -462,16 +462,16 @@ stack_blocks_no_pick_and_place = [
     Gripper now open.
     gripper: None
     return to hover:  ([0.102, -0.358, 0.216], [0.0, 0.0, 0.0])
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The middle block was successfully placed on the largest block. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows the middle block successfully placed on the largest block.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The middle block is now placed on the largest block. I am still in step 1, stacking the blocks steadily in order of size.
@@ -494,7 +494,7 @@ stack_blocks_no_pick_and_place = [
     print("place pos: ", move_robot(middle_block_pos, [0, 0, 0]))
     print("gripper: ", open_gripper())
     print("move away: ", move_robot(hover_pos, [0, 0, 0]))
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
@@ -510,7 +510,7 @@ stack_blocks_no_pick_and_place = [
     Gripper now open.
     gripper: None
     return to hover:  ([0.097, -0.359, 0.232], [0.0, 0.0, 0.0])
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 ]
 
 knock_over_all_modules = [
@@ -529,7 +529,7 @@ knock_over_all_modules = [
     print("gripper: ", close_gripper())
     print("side pos: ", move_robot(side_pos, [0, 0, 0]))
     print("knock over: ", move_robot_relative([0.2, 0, 0], [0, 0, 0]))
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
@@ -538,7 +538,7 @@ knock_over_all_modules = [
     gripper: None
     side pos:  ([0.197, -0.359, 0.132], [0.0, 0.0, 0.0])
     knock over:  ([0.397, -0.359, 0.132], [0.0, 0.0, 0.0])
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Looking at the positions the robot moved to, I made a mistake in the relative movement.
@@ -547,13 +547,13 @@ knock_over_all_modules = [
     $$CODE$$
     print("side pos: ", move_robot(side_pos, [0, 0, 0]))
     print("knock over: ", move_robot_relative([-0.2, 0, 0], [0, 0, 0]))
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
     side pos:  ([0.197, -0.359, 0.132], [0.0, 0.0, 0.0])
     knock over:  ([-0.003, -0.359, 0.132], [0.0, 0.0, 0.0])
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 ]
 
 knock_over_no_fine_control = [
@@ -570,13 +570,13 @@ knock_over_no_fine_control = [
     # I will try picking up the middle block and 'placing' it above the tower.
     above_middle_block_pos = [middle_block_pos[0], middle_block_pos[1], middle_block_pos[2] + 0.1]
     success = put_first_on_second(middle_block_pos, above_middle_block_pos)
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
     middle block position:  [0.097, -0.359, 0.132]
     True
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 ]
 
 # user, [assistant, system]*
@@ -584,13 +584,13 @@ def construct_example(stack_blocks, knock_over):
     return [
     '''
     Please stack the blocks steadily and then, when you see an egg, knock them over.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     gptv_injection_message,
 
     '''
     Image redacted in example. Image shows scene with 3 blocks of different sizes.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Contextual observation: There are three blocks and they are each different sizes. However, all three are small enough they should be easy to pick up with a vertical gripper.
@@ -609,23 +609,23 @@ def construct_example(stack_blocks, knock_over):
     $$CODE$$
     block_names = vision_assistant("Please order the three blocks by size, largest first. Please return a list of strings with the names of the blocks. I believe there is one each of a red, blue, and green block.")
     print("block names: "block_names)
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
     block names: ['red', 'green', 'blue']
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     *stack_blocks,
 
     '''
     I think the blocks were successfully stacked. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows the smallest block successfully placed on the middle block.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The blocks are now stacked in order of size. I am now finished with step 1.
@@ -641,7 +641,7 @@ def construct_example(stack_blocks, knock_over):
         print(egg)
         time.sleep(5)
         count += 1
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     stdout:
@@ -651,34 +651,34 @@ def construct_example(stack_blocks, knock_over):
     False
     False
     True
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     I think there is an egg in the scene. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows an egg in the scene.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     *knock_over,
 
     '''
     I think the blocks were successfully knocked over. I will now check the scene to verify.
     $$VIEW_SCENE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     Image redacted in example. Image shows the blocks knocked over.
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 
     '''
     The blocks are now knocked over. I am now finished with step 3.
     The user's original task was to stack the blocks steadily and then, when you see an egg, knock them over.
     I have completed the task and verified the scene. I will now give a final message.
     $$COMPLETE$$
-    '''.strip(),
+    '''.strip().replace('\n    ', '\n'),
 ]
 
 all_modules_example = construct_example(stack_blocks_all_modules, knock_over_all_modules)
